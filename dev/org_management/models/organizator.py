9cac54f4679501b0d03bd938b1b50e5d1beca18f
Irina <nerhneiro@gmail.com>
2024-01-16 22:34:27 +0300

    Add fields to  organizator model

--- dev/org_management/models/organizator.py
+++ dev/org_management/models/organizator.py
@@ -2,13 +2,13 @@ from odoo import api, fields, models
 class Organizator(models.Model):
     _name = "management.organizator"
 
-    # _description = "Organizator information"
+    _description = "Organizator information"
 
-    # name = fields.Char(string="Name", required=True)
-    # surname = fields.Char(string="Surname", required=True)
-    # second_name = fields.Char(string="Second name", required=False)
-    # is_adult = fields.Boolean(string="Is over 18?")
-    # gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string="Gender")
+    name = fields.Char(string="Name", required=True)
+    surname = fields.Char(string="Surname", required=True)
+    second_name = fields.Char(string="Second name", required=False)
+    is_adult = fields.Boolean(string="Is over 18?")
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string="Gender")
 
 
     #name = fields.Many2many()
