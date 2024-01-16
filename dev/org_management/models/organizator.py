b15db19f123c3be37a4cd37b9f6c8613cd244820
Irina <nerhneiro@gmail.com>
2024-01-16 22:51:35 +0300

    Add fields to organizator model

--- dev/org_management/models/organizator.py
+++ dev/org_management/models/organizator.py
@@ -9,6 +9,7 @@ class Organizator(models.Model):
     second_name = fields.Char(string="Second name", required=False)
     is_adult = fields.Boolean(string="Is over 18?")
     gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string="Gender")
-
+    phone_number = fields.Char(string="Phone number")
+    email = fields.Char(string="Email")
 
     #name = fields.Many2many()
