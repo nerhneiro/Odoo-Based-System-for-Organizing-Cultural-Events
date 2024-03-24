a620c25e081dfad65cdf836c74e2c0430b2e5866
Irina <nerhneiro@gmail.com>
2024-03-24 18:49:04 +0300

    org_management.group_event_organizer is set by default for newly authenticated users

---
+++ dev/org_management/controllers/main.py
@@ -0,0 +1,16 @@
+from odoo import http
+from odoo.http import request
+from odoo.addons.auth_signup.controllers.main import AuthSignupHome
+
+class BecomeOrganizerOnSignUp(AuthSignupHome): #это override sign up page
+
+    def do_signup(self, *args):
+        super(BecomeOrganizerOnSignUp, self).do_signup(*args) #совершается регистрация пользователя
+        env = request.env
+        env.user.sudo().write({'groups_id': [
+            (3, env.ref('base.group_portal').id), #удаляю пользователя из группы (типа) "Portal user", так как по дефолту приписывается она, и "User can't have two group types"
+            (4, env.ref('base.group_user').id), #добавляю пользователя в группу "Internal user"
+            (4, env.ref('org_management.group_event_organizer').id), #приписываю группу "group_event_organizer" при регистрации
+        ]
+        })
+        request.env.cr.commit()
